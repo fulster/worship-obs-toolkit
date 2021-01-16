1a26bb209cea5f658926e7e9b1682b118bf40dfc
fulster <etienne@magic.Fr>
2021-01-16 23:44:02 +0100

    Big refactoring

--- generate.py
+++ generate.py
@@ -1,67 +1,21 @@
+import json
+import os
+from obs_json_resources import Obs_basic
 from obs_json_resources import Scene_Collection
 from obs_json_resources import Scene
-from obs_json_resources import Src_cantique_title
-from obs_json_resources import Itm_cantique_title
-from obs_json_resources import Src_cantique_paroles
-from obs_json_resources import Itm_cantique_paroles
 
-import json
+name = "Dimanche 17 janvier 2021 YESSSS"
 
-def prepare_cantique(collec, name) :
-    MyScene=Scene(name)
-    MyCantique_title = Src_cantique_title(name)
-    MyItemCantique_title = Itm_cantique_title(name)
-    MyCantique_paroles = Src_cantique_paroles(name)
-    MyItemCantique_paroles = Itm_cantique_paroles(name)
-
-
-    MyScene.add_item(MyItemCantique_title.to_json())
-    Mycollect.add_source(MyCantique_title.to_json())
-    MyScene.add_item(MyItemCantique_paroles.to_json())
-    Mycollect.add_source(MyCantique_paroles.to_json())
-
-    Mycollect.add_source(MyScene.to_json())
-
-
-def prepare_scene(data, filename) :     
-    pass
-    #load cantique from external file
-    #head=''
-    #with open(filename,"r",encoding="utf-8") as cantique :
-        #head = ''.join([next(cantique) for x in range(2)])
-    #    fileContents = '\n\n\n\n'+cantique.read()
-
-    #manage title
-    #cantique_title["name"]=filename+': titre'
-    #cantique_title["settings"]["text"]=head
-    #data["sources"].append(cantique_title)
-
-    #title_into_scene["name"]= filename+': titre'
-
-    #create new cantique source
-    #src_txt["name"]=filename+': paroles'
-    #src_txt["settings"]['text']=fileContents
-    #data["sources"].append(src_txt)
-
-    #txt_into_scene["name"]=filename+': paroles'
-
-    #create new scene source
-    #scene_item["name"] = 'scene : '+filename
-    #scene_item["settings"]["items"].append(txt_into_scene)
-    #scene_item["settings"]["items"].append(title_into_scene)
-    #data["sources"].append(scene_item)
-    #return data
-    
-#open template into data object
-#with open('Template.json',"r",encoding="utf-8") as template:
-#    data = json.load(template)
-
-name = "Dimanche 17 janvier 2021"
-Mycollect=Scene_Collection(name)
-
-prepare_cantique(Mycollect,"Frappez dans vos mains")
-prepare_cantique(Mycollect,"Viens, Saint-Esprit")
-prepare_cantique(Mycollect,"Si vous saviez quel Sauveur")
+collection = Scene_Collection(name)
+collection.generate_scenes_from_dir('./txt')
+# collection.add_scene('./txt/21-07 Qu’aujourd’hui toute la terre.txt')
+# collection.add_scene('./txt/Psaume 36 O Seigneur, ta fidélité.txt')
+# collection.add_scene('./txt/(spontané) 36-12 O notre Dieu, nous te prions.txt')
+# collection.add_scene('./txt/(spontané) 42-02 Du cœur et de la voix.txt')
+# collection.add_scene('./txt/(spontané) 45-01 Ta volonté, Seigneur mon Dieu.txt')
+# collection.add_scene('./txt/22-05 Dans ta parole, Ô Dieu.txt')
+# collection.add_scene('./txt/46-05 Mon sauveur, je voudrais être.txt')
+# collection.add_scene('./txt/36-08 O Jésus, tu nous appelles.txt')
 
 with open(name+".json", 'w') as outfile:
-        json.dump(Mycollect.to_json(), outfile)
+        json.dump(collection.to_json(), outfile)
--- obs_json_resources.py
+++ obs_json_resources.py
@@ -1,70 +1,109 @@
+import os
 import json
 
-class Src_cantique_title :
+
+class Obs_basic :
     def __init__(self,name) :
-        with open('cantique_title.json',"r",encoding="utf-8") as template:
-            self.core = json.load(template)
-        #load cantique from external file
-        head=''
-        with open(name+'.txt',"r",encoding="utf-8") as cantique :
-            head = ''.join([next(cantique) for x in range(2)])
-            self.core["settings"]["text"]=head
-        self.core["name"]=name+" : titre"
-
-    def to_json(self):
-        return self.core
-
-class Src_cantique_paroles :
-    def __init__(self,name) :
-        with open('cantique_paroles.json',"r",encoding="utf-8") as template:
-            self.core = json.load(template)
-        #load cantique from external file
-        with open(name+'.txt',"r",encoding="utf-8") as cantique :
-            ''.join([next(cantique) for x in range(2)])
-            data = '\n\n\n\n'+cantique.read()
-            self.core["settings"]["text"]=data
-        self.core["name"]=name+" : paroles"
-
-    def to_json(self):
-        return self.core        
-
-class Itm_cantique_title :
-    def __init__(self,name) :
-        with open('cantique_title_item.json',"r",encoding="utf-8") as template:
-            self.core = json.load(template)
-        #load cantique from external file
-        self.core["name"]=name+" : titre"
-
-    def to_json(self):
-        return self.core
-
-class Itm_cantique_paroles :
-    def __init__(self,name) :
-        with open('cantique_paroles_item.json',"r",encoding="utf-8") as template:
-            self.core = json.load(template)
-        #load cantique from external file
-        self.core["name"]=name+" : paroles"
-
-    def to_json(self):
-        return self.core
-
-class Scene :
-    def __init__(self,name) :
-        with open('basic_scene.json',"r",encoding="utf-8") as template:
+        class_name = self.__class__.__name__
+        dir = os.path.dirname(__file__)
+        tpl = os.path.abspath(dir+'/tpl/'+class_name+'.json')
+        with open(tpl,"r",encoding="utf-8") as template:
             self.core = json.load(template)
         self.core["name"]=name
     def to_json(self):
         return self.core
+
+
+# class Src_cantique_paroles :
+#     def __init__(self,name) :
+#         with open('cantique_paroles.json',"r",encoding="utf-8") as template:
+#             self.core = json.load(template)
+#         #load cantique from external file
+#         with open(file,"r",encoding="utf-8") as cantique :
+#             ''.join([next(cantique) for x in range(2)])
+#             data = '\n\n\n\n'+cantique.read()
+#             self.core["settings"]["text"]=data
+#         self.core["name"]=name+" : paroles"
+
+#     def to_json(self):
+#         return self.core        
+
+
+
+# class Itm_cantique_paroles :
+#     def __init__(self,name) :
+#         with open('cantique_paroles_item.json',"r",encoding="utf-8") as template:
+#             self.core = json.load(template)
+#         #load cantique from external file
+#         self.core["name"]=name+" : paroles"
+
+#     def to_json(self):
+#         return self.core
+
+class Cantique_lyrics_item(Obs_basic) :
+    def __init__(self,name) :
+        self.name=name+" : lyrics"
+        super().__init__(self.name)
+
+class Cantique_lyrics_source(Obs_basic) :
+    def __init__(self,name,content) :
+        self.name=name+" : lyrics"
+        super().__init__(self.name)
+        self.core["settings"]["text"]=content
+
+
+class Cantique_title_item(Obs_basic) :
+    def __init__(self,name) :
+        self.name=name+" : titre"
+        super().__init__(self.name)
+
+class Cantique_title_source(Obs_basic) :
+    def __init__(self,name,content) :
+        self.name=name+" : titre"
+        super().__init__(self.name)
+        self.core["settings"]["text"]=content
+
+
+# a reprendre sur une classe plus précise
+class Scene(Obs_basic) :
+    def __init__(self, file,sc):
+        self.file = os.path.abspath(file)
+        with open(file,"r",encoding="utf-8") as cantique :
+            self.head = ''.join([next(cantique) for x in range(2)])
+            self.lyrics = '\n\n\n\n'+cantique.read()    
+        base=os.path.basename(file)
+        self.name = os.path.splitext(base)[0]
+        super().__init__(self.name)
+
+        #title
+        cantique_title_source=Cantique_title_source(self.name,self.head)
+        cantique_title_item=Cantique_title_item(self.name)
+        sc.add_source(cantique_title_source.to_json())
+        self.add_item(cantique_title_item.to_json())
+
+        #lyrics
+        cantique_lyrics_source=Cantique_lyrics_source(self.name,self.lyrics)
+        cantique_lyrics_item=Cantique_lyrics_item(self.name)
+        sc.add_source(cantique_lyrics_source.to_json())
+        self.add_item(cantique_lyrics_item.to_json())
+
+        #the scene itself
+        sc.add_source(self.to_json())
+
     def add_item(self,item) :
         self.core["settings"]["items"].append(item)
 
 
-class Scene_Collection :
-    def __init__(self,name) :
-        with open('Template.json',"r",encoding="utf-8") as template:
-            self.core = json.load(template)
-        self.core["name"]=name
-    def to_json(self):
-        return self.core
+class Scene_Collection(Obs_basic) :
+    def __init__(self,name):
+        super().__init__(name)
+        self.scenes=[]
     def add_source(self,source) :
         self.core["sources"].append(source)
+    def add_scene(self,file) :
+        self.scenes.append(Scene(file,self))
+    def generate_scenes_from_dir(self, dir) :
+        filelist = os.listdir(os.path.abspath(dir)) # returns list
+        for file in filelist:
+            self.add_scene(os.path.abspath(dir+"/"+file))
+            #  prepare_cantique(scene,os.path.abspath(dir+"/"+file))
